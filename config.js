1c65847d4107b6bc7cfd051bb82cc08611d2a522
godleader <kenny9@gmail.com>
2025-09-09 09:45:57 +0800

    init

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
   mongodbUri: process.env.MONGODB_URI,
   port: process.env.PORT || 8080,
   sessionSecret: process.env.SESSION_SECRET || 'your-secret-key',
-  renderUrl: process.env.RENDER_URL,
+  renderUrl: 'https://dbcheck-pqur.onrender.com',
   botUsername: process.env.BOT_USERNAME || 'your_bot_username',
   hlrApiKey: process.env.HLR_API_KEY,
 };
